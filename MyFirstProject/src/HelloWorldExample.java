1aaa6ddaebdf2df4676b3134263a96cc1e745f67
RogerRatu <rodgaratu@windowslive.com>
2019-03-13 10:13:54 +1300

    Testing out New Branch Activity

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to GitHub user 
+		// Output Hello World to GitHub User RogerRatu
 		System.out.println("Hello World");
 
 	}
