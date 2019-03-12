1772dc4b81a1f2f755c5bab18ad35d147e394970
RogerRatu <rodgaratu@windowslive.com>
2019-03-13 09:26:11 +1300

    updated comment

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to user 
+		// Output Hello World to GitHub User
 		System.out.println("Hello World");
 
 	}
